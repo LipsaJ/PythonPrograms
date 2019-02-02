e04d9957f79b8010691cd315b00e837bc8c915c8
Lipsa Jena <41341559+LipsaJ@users.noreply.github.com>
2019-02-02 21:54:23 +0530

    hello worrld python

---
+++ HelloWorld/App.py
@@ -0,0 +1,21 @@
+#for next video (24)
+#put cntl+/ for multiple code lines comment
+# greetings = "Hello"
+# #this is to to take input from prompt
+# name = input("Please enter your name")
+# print(greetings + ' ' +name)
+
+print("This line is\nbroken at \nvarious places.")
+#\n adds new line
+print("1\t2\t3\t4\t5")#\t for tab
+print('This is Yashu\'s Sofa')#if you want to use same quotes within quotes then use \
+anotherSplitString = """This Line Is
+Broken
+at so many places"""
+print(anotherSplitString)
+#one of the special use of python for three quotes
+anotherString = """He's gone outside for buying
+"Clothes" which he doesn't need
+at all."""
+print(anotherString)
+#within three quotes you can use both single and double quotes as well as new line
